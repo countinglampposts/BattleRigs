df300f018b294f92979145ae33096b062b99d1ef
Chris <countinglampposts@gmail.com>
2018-11-08 09:41:54 -0500

    Initial commity

---
+++ Assets/Scripts/EasySuspension.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+public class EasySuspension : MonoBehaviour
+{
+    [Range(0.1f, 20f)]
+    [Tooltip("Natural frequency of the suspension springs. Describes bounciness of the suspension.")]
+    public float naturalFrequency = 10;
+
+    [Range(0f, 3f)]
+    [Tooltip("Damping ratio of the suspension springs. Describes how fast the spring returns back after a bounce. ")]
+    public float dampingRatio = 0.8f;
+
+    [Range(-1f, 1f)]
+    [Tooltip("The distance along the Y axis the suspension forces application point is offset below the center of mass")]
+    public float forceShift = 0.03f;
+
+    [Tooltip("Adjust the length of the suspension springs according to the natural frequency and damping ratio. When off, can cause unrealistic suspension bounce.")]
+    public bool setSuspensionDistance = true;
+
+    [Tooltip("If you need the visual wheels to be attached automatically, drag the wheel shape here.")]
+    public GameObject wheelShape;
+
+    Rigidbody m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        var m_Wheels = GetComponentsInChildren<WheelCollider>();
+
+        for (int i = 0; i < m_Wheels.Length; ++i)
+        {
+            var wheel = m_Wheels[i];
+
+            // Create wheel shapes only when needed.
+            if (wheelShape != null)
+            {
+                var ws = Instantiate(wheelShape);
+                ws.transform.parent = wheel.transform;
+            }
+        }
+    }
+
+    void Update()
+    {
+        // Work out the stiffness and damper parameters based on the better spring model.
+        foreach (WheelCollider wheel in GetComponentsInChildren<WheelCollider>())
+        {
+            JointSpring spring = wheel.suspensionSpring;
+
+            float sqrtWcSprungMass = Mathf.Sqrt(wheel.sprungMass);
+            spring.spring = sqrtWcSprungMass * naturalFrequency * sqrtWcSprungMass * naturalFrequency;
+            spring.damper = 2f * dampingRatio * Mathf.Sqrt(spring.spring * wheel.sprungMass);
+
+            wheel.suspensionSpring = spring;
+
+            Vector3 wheelRelativeBody = transform.InverseTransformPoint(wheel.transform.position);
+            float distance = m_Rigidbody.centerOfMass.y - wheelRelativeBody.y + wheel.radius;
+
+            wheel.forceAppPointDistance = distance - forceShift;
+
+            // Make sure the spring force at maximum droop is exactly zero
+            if (spring.targetPosition > 0 && setSuspensionDistance)
+                wheel.suspensionDistance = wheel.sprungMass * Physics.gravity.magnitude / (spring.targetPosition * spring.spring);
+
+
+            // Update visual wheels if any.
+            if (wheelShape)
+            {
+                Quaternion r;
+                Vector3 p;
+                wheel.GetWorldPose(out p, out r);
+
+                // Assume that the only child of the wheelcollider is the wheel shape.
+                Transform shapeTransform = wheel.transform.GetChild(0);
+                shapeTransform.position = p;
+                shapeTransform.rotation = r;
+            }
+        }
+    }
+
+    // Uncomment this to observe how parameters change.
+    /*
+    void OnGUI()
+    {
+        foreach (WheelCollider wc in GetComponentsInChildren<WheelCollider>()) {
+            GUILayout.Label (string.Format("{0} sprung: {1}, k: {2}, d: {3}", wc.name, wc.sprungMass, wc.suspensionSpring.spring, wc.suspensionSpring.damper));
+        }
+
+        GUILayout.Label ("Inertia: " + m_Rigidbody.inertiaTensor);
+        GUILayout.Label ("Mass: " + m_Rigidbody.mass);
+        GUILayout.Label ("Center: " + m_Rigidbody.centerOfMass);
+    }
+    */
+
+}
---
+++ Assets/Scripts/WheelDrive.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public enum DriveType
+{
+    RearWheelDrive,
+    FrontWheelDrive,
+    AllWheelDrive
+}
+
+public class WheelDrive : MonoBehaviour
+{
+    [Tooltip("Maximum steering angle of the wheels")]
+    public float maxAngle = 30f;
+    [Tooltip("Maximum torque applied to the driving wheels")]
+    public float maxTorque = 300f;
+    [Tooltip("Maximum brake torque applied to the driving wheels")]
+    public float brakeTorque = 30000f;
+
+    [Tooltip("The vehicle's speed when the physics engine can use different amount of sub-steps (in m/s).")]
+    public float criticalSpeed = 5f;
+    [Tooltip("Simulation sub-steps when the speed is above critical.")]
+    public int stepsBelow = 5;
+    [Tooltip("Simulation sub-steps when the speed is below critical.")]
+    public int stepsAbove = 1;
+
+    [Tooltip("The vehicle's drive type: rear-wheels drive, front-wheels drive or all-wheels drive.")]
+    public DriveType driveType;
+
+    private WheelCollider[] m_Wheels;
+
+    // Find all the WheelColliders down in the hierarchy.
+    void Start()
+    {
+        m_Wheels = GetComponentsInChildren<WheelCollider>();
+    }
+
+    // This is a really simple approach to updating wheels.
+    // We simulate a rear wheel drive car and assume that the car is perfectly symmetric at local zero.
+    // This helps us to figure our which wheels are front ones and which are rear.
+    void Update()
+    {
+        m_Wheels[0].ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
+
+        float angle = maxAngle * Input.GetAxis("Horizontal");
+        float torque = maxTorque * Input.GetAxis("Vertical");
+
+        float handBrake = Input.GetKey(KeyCode.X) ? brakeTorque : 0;
+
+        foreach (WheelCollider wheel in m_Wheels)
+        {
+            // A simple car where front wheels steer while rear ones drive.
+            if (wheel.transform.localPosition.z > 0)
+                wheel.steerAngle = angle;
+
+            if (wheel.transform.localPosition.z < 0)
+            {
+                wheel.brakeTorque = handBrake;
+            }
+
+            if (wheel.transform.localPosition.z < 0 && driveType != DriveType.FrontWheelDrive)
+            {
+                wheel.motorTorque = torque;
+            }
+
+            if (wheel.transform.localPosition.z >= 0 && driveType != DriveType.RearWheelDrive)
+            {
+                wheel.motorTorque = torque;
+            }
+        }
+    }
+}
